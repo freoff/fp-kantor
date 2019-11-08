a81fdef8890ffb8223f02842d1274596a5cd272b
Krzysztof Podlaski <kpodlaski@gmail.com>
2019-11-08 21:33:46 +0100

    run prettier on files

--- cypress/integration/start.js
+++ cypress/integration/start.js
@@ -1,15 +1,12 @@
-
-
 describe('Test Cypress work', () => {
   it('Cypress should work', () => expect(true).to.equal(true));
 });
 describe('Application ', () => {
-  it('should have correct title', (done) => {
+  it('should have correct title', done => {
     cy.visit(Cypress.env().applicationURL);
     cy.fixture('root').then(data => {
-      cy.title({log: true}).should('be.eq', data.applicationTitle);
+      cy.title({ log: true }).should('be.eq', data.applicationTitle);
       done();
-    })
+    });
   });
-
 });
